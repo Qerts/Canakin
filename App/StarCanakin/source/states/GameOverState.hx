41d7849dc9c090606d61092ee2ba4d7838c9698f
Bohdan Taraba <tbohdan@seznam.cz>
2015-05-11 18:15:17 +0200

    zmenena kompozice kodu pro staty
    pridana slozka, presunute tridy, pridane tridy pro staty gameover a
    retry

---
+++ App/StarCanakin/source/states/GameOverState.hx
@@ -0,0 +1,21 @@
+package states;
+import flixel.FlxState;
+
+/**
+ * ...
+ * @author w
+ */
+class GameOverState extends FlxState
+{
+
+	public function new() 
+	{
+		super();
+	}
+	
+	override public function create():Void 
+	{
+		super.create();
+	}
+	
+}
---
+++ App/StarCanakin/source/states/NextBattleState.hx
@@ -0,0 +1,20 @@
+package states;
+import flixel.FlxState;
+
+/**
+ * ...
+ * @author w
+ */
+class NextBattleState extends FlxState
+{
+
+	public function new() 
+	{
+		super();
+	}
+	
+	override public function create():Void 
+	{
+		super.create();
+	}
+}
